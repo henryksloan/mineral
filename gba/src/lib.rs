eb6b1818008992baae68fa907dd6b62268d198a1
Henry Sloan <henryksloan@gmail.com>
2021-04-23 17:18:57 -0400

    Flesh out integrated skeleton code

--- gba/src/lib.rs
+++ gba/src/lib.rs
@@ -1,65 +1,85 @@
+mod io_controller;
+
+use crate::io_controller::IoController;
+
 use cpu::CPU;
 use memory::{MMU, RAM, ROM};
+use ppu::PPU;
 
 use std::cell::RefCell;
 use std::rc::Rc;
 
 pub struct GBA {
-    cpu: CPU,
+    cpu: Rc<RefCell<CPU>>,
+    ppu: Rc<RefCell<PPU>>,
 
     bios_rom: Rc<RefCell<ROM<0x4000>>>,   // BIOS ROM
     ewram: Rc<RefCell<RAM<0x40000>>>,     // Internal work RAM
     iwram: Rc<RefCell<RAM<0x8000>>>,      // External work RAM
     vram: Rc<RefCell<RAM<0x18000>>>,      // VRAM
     palette_ram: Rc<RefCell<RAM<0x400>>>, // Palette RAM
+    oam: Rc<RefCell<RAM<0x400>>>,         // Object attribute memory
     cart_rom: Rc<RefCell<ROM<0x400000>>>, // Cartridge ROM
 
     mmu: Rc<RefCell<MMU>>, // Defines the CPU/DMA address space
+    io_controller: Rc<RefCell<IoController>>,
 }
 
 impl GBA {
     pub fn new() -> Self {
-        // TODO: Initialize interrupt and IO controllers
-
         let bios_rom = Rc::new(RefCell::new(ROM::new()));
         let ewram = Rc::new(RefCell::new(RAM::new()));
         let iwram = Rc::new(RefCell::new(RAM::new()));
         let vram = Rc::new(RefCell::new(RAM::new()));
         let palette_ram = Rc::new(RefCell::new(RAM::new()));
+        let oam = Rc::new(RefCell::new(RAM::new()));
         let cart_rom = Rc::new(RefCell::new(ROM::new()));
 
         let mmu = Rc::new(RefCell::new(MMU::new()));
+
+        // TODO: Initialize the DMA controller with the MMU Rc
+
+        let cpu = Rc::new(RefCell::new(CPU::new())); // TODO: Pass a clone of the MMU Rc to CPU
+        let ppu = Rc::new(RefCell::new(PPU::new(
+            vram.clone(),
+            palette_ram.clone(),
+            oam.clone(),
+        )));
+
+        let io_controller = Rc::new(RefCell::new(IoController::new(ppu.clone())));
+        // TODO: Initialize interrupt controller
+
+        // Populate the address space
         {
             let mut mmu_mut = mmu.borrow_mut();
             mmu_mut.map_range(0x00000000..=0x00003FFF, bios_rom.clone());
             mmu_mut.map_range(0x02000000..=0x0203FFFF, ewram.clone());
             mmu_mut.map_range(0x03000000..=0x0307FFFF, iwram.clone());
-            // mmu_mut.map(0x04000000..=0x040003FE, io_controller.clone());
+            mmu_mut.map_range(0x04000000..=0x040003FE, io_controller.clone());
             mmu_mut.map_range(0x05000000..=0x050003FF, palette_ram.clone());
             mmu_mut.map_range(0x06000000..=0x06017FFF, vram.clone());
             mmu_mut.map_range(0x08000000..=0x0DFFFFFF, cart_rom.clone());
         }
 
-        // TODO: Initialize the DMA controller with the MMU Rc
-
-        let cpu = CPU::new(); // TODO: Pass a clone of the MMU Rc to CPU
-
-        // TODO: Create the PPU with a clone of the VRAM, OAM Rc's
-
         Self {
             cpu,
+            ppu,
+
             bios_rom,
             ewram,
             iwram,
             vram,
             palette_ram,
+            oam,
             cart_rom,
+
             mmu,
+            io_controller,
         }
     }
 
     pub fn tick(&mut self) {
-        self.cpu.tick();
+        self.cpu.borrow_mut().tick();
 
         // if !dma_controller.is_active() {
         //     self.cpu.tick()
--- ppu/src/lib.rs
+++ ppu/src/lib.rs
@@ -1,1 +1,75 @@
-pub struct PPU {}
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use memory::Memory;
+
+pub struct PPU {
+    vram: Rc<RefCell<dyn Memory>>,
+    palette_ram: Rc<RefCell<dyn Memory>>,
+    oam: Rc<RefCell<dyn Memory>>,
+
+    scan_line: u8,
+    scan_cycle: u32,
+
+    bg_mode: u16, // TODO: Enum
+
+    framebuffer: [u8; 240 * 160 * 2],
+}
+
+impl PPU {
+    pub fn new(
+        vram: Rc<RefCell<dyn Memory>>,
+        palette_ram: Rc<RefCell<dyn Memory>>,
+        oam: Rc<RefCell<dyn Memory>>,
+    ) -> Self {
+        Self {
+            vram,
+            palette_ram,
+            oam,
+
+            scan_line: 0,
+            scan_cycle: 0,
+
+            bg_mode: 0,
+
+            framebuffer: [0; 240 * 160 * 2],
+        }
+    }
+
+    pub fn tick(&mut self) {
+        if self.scan_cycle == 0 {
+            self.draw_scanline();
+        }
+        self.increment_scan();
+    }
+
+    pub fn vcount(&self) -> u8 {
+        self.scan_line
+    }
+
+    fn increment_scan(&mut self) {
+        self.scan_cycle = (self.scan_cycle + 1) % 1232;
+
+        if self.scan_cycle == 0 {
+            self.scan_line = (self.scan_line + 1) % 228;
+
+            if self.scan_line == 160 {
+                // TODO: VBlank interrupt if enabled
+            }
+
+            // TODO: V-Counter interrupt if enabled
+        } else if self.scan_cycle == 960 {
+            // TODO: HBlank interrupt if enabled
+        }
+    }
+
+    fn draw_scanline(&mut self) {
+        // TODO: Should generate a line from each active layer (backgrounds, obj),
+        // taking into account windowing, then combine them based on priority and blending
+        // match self.bg_mode {
+        //     0 => {
+        //
+        //     }
+        // }
+    }
+}
